5a1efd08615fd38d6c72deac33146c4dd059d02e
Игорь Богачев <bogachigor1@yandex.ru>
2024-09-07 14:19:56 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
+def go():
+    return redirect("/menu", code=302)
 @app.route("/index")
 def start():
+    return redirect("/menu", code=302)
+
+@app.route("/menu")
+def menu():
     return """
 <!doctype html>
 <html>
