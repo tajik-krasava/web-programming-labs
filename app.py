5bd80d37f07e0773e49212f56515c17f91b76a0e
Игорь Богачев <bogachigor1@yandex.ru>
2024-09-07 13:59:05 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,32 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body> 
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <ul>
+        <li><a href="/lab1">lab1</a></li>
+        </ul>
+
+        <footer>
+            &copy; Игорь Богачев, ФБИ-23, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -15,6 +40,12 @@ def start():
         </header>
 
         <h1>web-сервер на flask</h1>
+        
+        <p>Flask — фреймворк для создания веб-приложений на языке
+программирования Python, использующий набор инструментов
+Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые ба-
+зовые возможности.
 
         <footer>
             &copy; Игорь Богачев, ФБИ-23, 3 курс, 2023
