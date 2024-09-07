3db81470974d0a18c94e40d775c7082f2e4fc7d1
Игорь Богачев <bogachigor1@yandex.ru>
2024-09-07 13:18:45 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Богачев Игорь Андреевич, лабораторная 1</title>
+    </head>
+    <body> 
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
 
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Игорь Богачев, ФБИ-23, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
