edf5942b9b3d40de475605c0d0b54f6841801d5a
Игорь Богачев <bogachigor1@yandex.ru>
2024-11-02 13:34:12 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2 
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.route("/")
 def go():
@@ -35,6 +37,7 @@ def menu():
         <li><a href="/lab1">Лабораторная работа 1</a></li>
         <li><a href="/lab2">Лабораторная работа 2</a></li>
         <li><a href="/lab3">Лабораторная работа 3</a></li>
+        <li><a href="/lab4">Лабораторная работа 4</a></li>
     </ul>
         <footer>
             &copy; Игорь Богачев, ФБИ-23, 3 курс, 2024
