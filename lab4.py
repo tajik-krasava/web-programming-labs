ac0b8fe338ef5118e11bdbcd3715aee414a48231
Игорь Богачев <bogachigor1@yandex.ru>
2024-12-20 01:45:33 +0900

    Деление

--- lab4.py
+++ lab4.py
@@ -4,3 +4,87 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4/lab4.html')
+
+@lab4.route('/lab4/div-form')
+def div_form():
+    return render_template('lab4/div-form.html')
+
+@lab4.route('/lab4/div', methods = ['POST'])
+def div():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/div.html', error='Оба поля должны быть заполнены!')
+    if x2 == '0':
+        return render_template('lab4/div.html', error='на ноль делить нельзя!')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1/x2
+    return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+@lab4.route('/lab4/sum', methods = ['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = '0'
+    if x2 == '':
+        x2 = '0'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/umn-form')
+def umn_form():
+    return render_template('lab4/umn-form.html')
+
+@lab4.route('/lab4/umn', methods = ['POST'])
+def umn():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = '1'
+    if x2 == '':
+        x2 = '1'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 * x2
+    return render_template('lab4/umn.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/vch-form')
+def vch_form():
+    return render_template('lab4/vch-form.html')
+
+@lab4.route('/lab4/vch', methods = ['POST'])
+def vch():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/vch.html', error='Оба поля должны быть заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/vch.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/vozv-form')
+def vozv_form():
+    return render_template('lab4/vozv-form.html')
+
+@lab4.route('/lab4/vozv', methods = ['POST'])
+def vozv():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/vozv.html', error='Оба поля должны быть заполнены!')
+    if x1 == '0' and x2 == '0':
+        return render_template('lab4/vozv.html', error='не должно быть 0!')
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 ** x2
+    return render_template('lab4/vozv.html', x1=x1, x2=x2, result=result)
