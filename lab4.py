4f41ff78c0f0d398168652d4e8b75231927a58ea
Игорь Богачев <bogachigor1@yandex.ru>
2024-12-20 03:06:11 +0900

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -106,6 +106,13 @@ def tree():
             tree_count += 1
     return redirect('/lab4/tree')
 
+users = [
+    {'login': 'ivan', 'password': '123'},
+    {'login': 'misha', 'password': '456'},
+    {'login': 'igor', 'password': 'igor'},
+    {'login': 'roma', 'password': '132'},
+]
+
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -114,8 +121,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
 
     error = 'Неверный логин и/или пароль'
     return render_template('/lab4/login.html', error=error, authorized=False)
