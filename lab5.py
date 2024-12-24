5affa11e59b9a8aa61cb242f1dbebd9bc8e25c70
Игорь Богачев <bogachigor1@yandex.ru>
2024-12-25 03:03:00 +0900

    Улучшения

--- lab5.py
+++ lab5.py
@@ -9,6 +9,15 @@ lab5 = Blueprint('lab5', __name__)
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
+def db_connect():
+    conn = psycopg2.connect(dbname="igor_bogachev_knowledge_base", user="postgres", password="admin", host="127.0.0.1")
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @lab5.route('/lab5/list')
 def list_articles():
@@ -29,15 +38,15 @@ def login():
     if not (login or password):
         return render_template('lab5/login.html', error="Заполните поля")
 
-    conn = psycopg2.connect(dbname="igor_bogachev_knowledge_base", user="postgres", password="admin", host="127.0.0.1"
-                            )
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute(f"SELECT * FROM users WHERE login='{login}'")
+    if not (login and password):
+        return render_template('lab5/login.html', error="Заполните поля")
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s", (login,))
     user = cur.fetchone()
 
-    if not user:
-        cur.close()
-        conn.close()
+    if not user or user['password'] != password:
         return render_template('lab5/login.html',
                                error='Логин и/или пароль неверны')
 
@@ -64,30 +73,20 @@ def register():
 
     login = request.form.get('login')
     password = request.form.get('password')
-    if not (login and password):
-        return render_template('lab5/register.html', error="Заполните все поля")
+    if not (login or password):
+        return render_template('lab5/register.html', error='Заполните все поля')
 
-    try:
-        conn = psycopg2.connect(dbname="igor_bogachev_knowledge_base",
-                                user="postgres", password="admin", host="127.0.0.1")
-        cur = conn.cursor()
-        cur.execute("SELECT login FROM users WHERE login=%s", (login,))
-        if cur.fetchone():
-            cur.close()
-            conn.close()
-            return render_template('lab5/register.html', error="Такой пользователь уже существует")
+    conn, cur = db_connect()
 
-        cur.execute(
-            "INSERT INTO users (login, password) VALUES (%s, %s);", (login, password))
-        conn.commit()
-    except Exception as e:
-        return render_template('lab5/register.html', error="Произошла ошибка при регистрации: " + str(e))
+    cur.execute(f"SELECT login FROM users WHERE login='{login}'")
+    if cur.fetchone():
+        db_close(conn, cur)
+        return render_template('lab5/register.html', error='Такой пользователь уже существует')
 
-    finally:
-        cur.close()
-        conn.close()
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}')")
 
-        return render_template('lab5/success.html', login=login)
+    db_close(conn, cur)
+    return render_template('lab5/success.html', login=login)
 
 
 @lab5.route('/lab5/success')
