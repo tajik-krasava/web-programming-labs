47772f8da71ee5a45ed5a81ab21d5d9b2d7c0960
Игорь Богачев <bogachigor1@yandex.ru>
2024-12-25 04:24:53 +0900

    Создание статей

--- lab5.py
+++ lab5.py
@@ -23,9 +23,27 @@ def list_articles():
     return 'Страница со списком статей'
 
 
-@lab5.route('/lab5/create')
+@lab5.route('/lab5/create', methods = ['GET', 'POST'])
 def create_article():
-    return 'Страница для создания статей'
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+
+    if request.method == 'GET':
+        return render_template('lab5/create.html')
+
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
+    login_id = cur.fetchone()["id"]
+
+    cur.execute(f"INSERT INTO aricles(user_id, title, article_text) VALUES ({login_id}, '{title}', '{article_text}')")
+
+    db_close(conn, cur)
+    return redirect('/lab5')
 
 
 @lab5.route('/lab5/login', methods=['GET', 'POST'])
