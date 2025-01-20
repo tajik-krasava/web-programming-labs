d0a0f9540a51ca8a68200da618dd636f33163ea5
Игорь Богачев <bogachigor1@yandex.ru>
2025-01-20 17:48:19 +0900

    Вывод, добавление и удаление фильмов

--- lab5.py
+++ lab5.py
@@ -14,8 +14,8 @@ def lab():
 
 def db_connect():
     if current_app.config['DB_TYPE'] == 'postgres':
-        conn = psycopg2.connect(dbname="igor_bogachev_knowledge_base",
-                                user="postgres", password="admin", host="127.0.0.1")
+        conn = psycopg2.connect(dbname="WEB",
+                                user="postgres", password="postgres", host="127.0.0.1")
         cur = conn.cursor(cursor_factory=RealDictCursor)
     else:
         dir_path = path.dirname(path.realpath(__file__))
---
+++ static/lab7/main.js
@@ -0,0 +1,94 @@
+function fillFilmList() {
+    fetch('/lab7/rest-api/films/')
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (films) {
+            let tbody = document.getElementById('film-list');
+            tbody.innerHTML = '';
+            for (let i = 0; i < films.length; i++) {
+                let tr = document.createElement('tr');
+                let tdTitle = document.createElement('td');
+                let tdTitleRus = document.createElement('td');
+                let tdYear = document.createElement('td');
+                let tdActions = document.createElement('td');
+                tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title;
+                tdTitleRus.innerText = films[i].title_ru;
+                tdYear.innerText = films[i].year;
+
+                let editButton = document.createElement('button');
+                editButton.innerText = 'Редактировать';
+
+
+                let delButton = document.createElement('button');
+                delButton.innerText = 'Удалить';
+                delButton.onclick = function () {
+                    deleteFilm(i, films[i].title);
+                };
+
+                tdActions.append(editButton);
+                tdActions.append(delButton);
+
+                tr.append(tdTitle);
+                tr.append(tdTitleRus);
+                tr.append(tdYear);
+                tr.append(tdActions);
+
+                tbody.append(tr);
+            }
+        })
+
+}
+function deleteFilm(id, title) {
+    if (!confirm(`Вы точно хотите удалить фильм "${title}"?`))
+        return;
+    fetch(`/lab7/rest-api/films/${id}`, { method: 'DELETE' })
+        .then(function () {
+            fillFilmList();
+        })
+}
+
+
+function showModal() {
+    document.querySelector('div.modal').style.display = 'block';
+}
+
+function hideModal() {
+    document.querySelector('div.modal').style.display = 'none';
+}
+
+function cancel() {
+    hideModal();
+}
+function addFilm() {
+    document.getElementById('id').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('title_ru').value = '';
+    document.getElementById('year').value = '';
+    document.getElementById('description').value = '';
+    showModal();
+}
+function sendFilm() {
+    const film = {
+        title: document.getElementById('title').value,
+        title_ru: document.getElementById('title_ru').value,
+        year: document.getElementById('year').value,
+        description: document.getElementById('description').value
+    }
+
+    const url = `/lab7/rest-api/films/`;
+    const method = 'POST';
+
+    fetch(url, {
+        method: method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(film)
+    })
+        .then(function () {
+            fillFilmList();
+            hideModal();
+        })
+        .catch(function (error) {
+            console.error('Ошибка при добавлении фильма:', error);
+        });
+}
