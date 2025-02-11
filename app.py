6d246d2a74f29811256259d6190287e69be21676
Игорь Богачев <bogachigor1@yandex.ru>
2025-02-11 23:11:29 +0900

    Модель базы данных

--- app.py
+++ app.py
@@ -8,12 +8,31 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 import os
+from os import path
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'cекретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE']=='postgres':
+    db_name = 'igor_bogachev_orm'
+    db_user = 'igor_bogachev_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path,"igor_bogachev_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
