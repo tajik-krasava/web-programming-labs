39b0eaa8ef078fcf9c24aca56b7b5e553c77fbb7
Игорь Богачев <bogachigor1@yandex.ru>
2025-02-13 05:59:47 +0900

    Открытка

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask(__name__)
 
@@ -54,6 +55,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 def go():
--- lab9.py
+++ lab9.py
@@ -1,7 +1,74 @@
-from flask import Blueprint, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, Blueprint
 
 lab9 = Blueprint('lab9', __name__)
 
-@lab9.route('/lab9/')
-def main():
-    return render_template('lab8/index.html')
+@lab9.route('/lab9/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        session['name'] = request.form['name']
+        return redirect(url_for('lab9.age'))
+    return render_template('lab9/index.html')
+
+@lab9.route('/lab9/age', methods=['GET', 'POST'])
+def age():
+    if request.method == 'POST':
+        session['age'] = request.form['age']
+        return redirect(url_for('lab9.gender'))
+    return render_template('lab9/age.html')
+
+@lab9.route('/lab9/gender', methods=['GET', 'POST'])
+def gender():
+    if request.method == 'POST':
+        session['gender'] = request.form['gender']
+        return redirect(url_for('lab9.preference'))
+    return render_template('lab9/gender.html')
+
+@lab9.route('/lab9/preference', methods=['GET', 'POST'])
+def preference():
+    if request.method == 'POST':
+        session['preference'] = request.form['preference']
+        return redirect(url_for('lab9.sub_preference'))
+    return render_template('lab9/preference.html')
+
+@lab9.route('/lab9/sub_preference', methods=['GET', 'POST'])
+def sub_preference():
+    if request.method == 'POST':
+        session['sub_preference'] = request.form['sub_preference']
+        return redirect(url_for('lab9.final'))
+    return render_template('lab9/sub_preference.html')
+
+@lab9.route('/lab9/final')
+def final():
+    name = session.get('name')
+    age = session.get('age')
+    gender = session.get('gender')
+    preference = session.get('preference')
+    sub_preference = session.get('sub_preference')
+  
+    if gender == 'male':
+        gender_pronoun = 'ты'
+        gift = 'мешочек конфет'
+        if preference == 'вкусное':
+            if sub_preference == 'сладкое':
+                message = f"Поздравляю тебя, {name}, желаю, хорошо встретить новый год. Вот тебе подарок — {gift}."
+                image = 'candy.jpg'  
+            else:
+                message = f"Поздравляю тебя, {name}, желаю, хорошо встретить новый год. Вот тебе подарок — сытное угощение."
+                image = 'savory.jpg'  
+        else:
+            message = f"Поздравляю тебя, {name}, желаю, хорошо встретить новый год. Вот тебе подарок — что-то красивое."
+            image = 'beautiful.jpg'  
+    else:
+        gender_pronoun = 'ты'
+        gift = 'тортик'
+        if preference == 'вкусное':
+            if sub_preference == 'сладкое':
+                message = f"Поздравляю тебя, {name}, желаю, хорошо встретить новый год. Вот тебе подарок — {gift}."
+                image = 'cake.jpg'  
+            else:
+                message = f"Поздравляю тебя, {name}, желаю, хорошо встретить новый год. Вот тебе подарок — сытное угощение."
+                image = 'savory.jpg'  
+        else:
+            message = f"Поздравляю тебя, {name}, желаю, хорошо встретить новый год. Вот тебе подарок — что-то красивое."
+            image = 'beautiful.jpg'  
+    return render_template('lab9/final.html', message=message, image=image)
